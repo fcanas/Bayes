97e2b8126592059a850d31933d4f9c17d406938d
Fabian Canas <fcanas@gmail.com>
2015-05-09 17:32:43 -0400

    Initial commit for multi-target Framework

---
+++ Bayes/Bayes.h
@@ -0,0 +1,14 @@
+//
+//  Bayes.h
+//  Bayes
+//
+//  Created by Fabian Canas on 5/9/15.
+//  Copyright (c) 2015 Fabian Canas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double BayesVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char BayesVersionString[];
+
