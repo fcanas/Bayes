894bcec1827e84db2af7e9464a92c74aad57e5e1
Eike Reinel <eike@agile-mindshift.de>
2020-10-07 14:58:09 +0200

    updated package description

--- Package.swift
+++ Package.swift
@@ -2,5 +2,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "Bayes"
+    name: "Bayes",
+    products: [
+            .library(name: "Bayes", targets: ["Bayes"]),
+        ],
+    targets: [
+            .target(
+                name: "Bayes",
+                dependencies: []),
+            .testTarget(
+                name: "BayesTests",
+                dependencies: ["Bayes"]),
+        ]
     )
